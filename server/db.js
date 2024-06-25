3c111c656ecf4ac40be8875ba82835fc9da302e0
jesse <jesse21532@gmail.com>
2024-06-25 21:38:43 +1200

    Fixed tab display size issue

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "milo123",
-    host: "localhost",
-    port: 5432,
-    database: "pern_pomodorro"
+  user: "postgres",
+  password: "milo123",
+  host: "localhost",
+  port: 5432,
+  database: "pern_pomodorro"
 });
 
 module.exports = pool;
