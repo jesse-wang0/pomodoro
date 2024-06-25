4c40b403a5d83dc41b940bf3e533d29ab4ce6de4
jesse <jesse21532@gmail.com>
2024-06-25 23:49:26 +1200

    Finished fuctionality for input box. Connected to db.

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useState } from "react";
+
+const InputTodo = () => {
+  const [description, setDescription] = useState("");
+  
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+      console.log(response)
+    } catch (err) {
+      console.error(err.message);
+    }
+  } 
+  return (
+    <Fragment>
+      <h1 className="text-center my-5">Input Todo</h1>
+      <form className="d-flex p-5" onSubmit={onSubmitForm}>
+        <input 
+          type="text" 
+          placeholder="Add todo" 
+          className="form-control mx-2"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
