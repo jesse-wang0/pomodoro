c05ec260f2bcfc4522e2fca68639d671c18f2148
jesse <jesse21532@gmail.com>
2024-07-08 00:08:39 +1200

    Added new command for study session database table.

--- server/database.sql
+++ server/database.sql
@@ -2,5 +2,12 @@ CREATE DATABASE pern_pomodorro;
 
 CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+    description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE study_sessions(
+    session_id SERIAL PRIMARY KEY,
+    duration INTEGER NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL
 );
