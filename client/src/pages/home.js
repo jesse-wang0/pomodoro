f3ff233c29cbe533ba61b9ed7577c3601132523f
jesse <jesse21532@gmail.com>
2024-07-17 00:06:08 +1000

    Inital commit. Page js files which will be used as elemenet by Route of the Router in the main App.js file.

---
+++ client/src/pages/home.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import TodoApp from "../components/todo_list/TodoApp";
+import TimerApp from "../components/timer/TimerApp";
+import Header from "../components/Header";
+import '../App.css';
+
+
+function Home() {
+  return (
+    <Fragment>
+      <Header />
+      <div class="main-app">
+        <TimerApp />
+        <TodoApp />
+      </div>
+    </Fragment>
+  )
+}
+
+export default Home;
---
+++ client/src/pages/statistics.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import Header from "../components/Header";
+
+function Statistics() {
+  return (
+    <Fragment>
+      <Header />
+      <h1>Statistics</h1>
+      <div id=""></div>
+      <div id="distribution-graph"></div>
+      <div id="bar-graph"></div>
+    </Fragment>
+  )
+}
+
+export default Statistics;
