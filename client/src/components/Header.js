075c1405f258fb61394499c748068400be6e14a5
jesse <jesse21532@gmail.com>
2024-07-27 13:08:42 +1000

    Finished functionality for header bar. Added 'logo' for the app and has 2 sections - timer and statistics.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,19 +4,19 @@ import './Header.css';
 
 function Header() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <Link to="/" class="navbar-brand rowdies-light" href="#timer">🍅 PomoTimer</Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand rowdies-light" href="#timer">🍅 PomoTimer</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <Link to="/" class="nav-link active" aria-current="page" >Timer</Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/" className="nav-link active" aria-current="page" >Timer</Link>
             </li>
-            <li class="nav-item">
-              <Link to="/statistics" class="nav-link active" aria-current="page" >Statistics</Link>
+            <li className="nav-item">
+              <Link to="/statistics" className="nav-link active" aria-current="page" >Statistics</Link>
             </li>
           </ul>
         </div>
