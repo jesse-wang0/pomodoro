6955bbe90bbfad4bf426586dcf179b8c6b1c6c15
jesse <jesse21532@gmail.com>
2024-07-31 01:06:47 +1000

    Cleanup code and delete empty lines.

--- client/src/components/todo_list/EditTodo.js
+++ client/src/components/todo_list/EditTodo.js
@@ -19,21 +19,21 @@ const EditTodo = ({ todo, refreshTodos }) => {
   }
   return (
     <Fragment>
-      <button type="button" className="btn btn-warning" data-bs-toggle="modal" 
+      <button type="button" className="btn btn-warning" data-bs-toggle="modal"
         data-bs-target={`#id${todo.todo_id}`}> Edit </button>
-      
+
       <div className="modal fade" id={`id${todo.todo_id}`} onClick={() => setDescription(todo.description)}>
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title">Edit Todo</h5>
-              <button type="button" className="btn-close" 
-              data-bs-dismiss="modal" aria-label="Close" 
-              onClick={() => setDescription(todo.description)}>
+              <button type="button" className="btn-close"
+                data-bs-dismiss="modal" aria-label="Close"
+                onClick={() => setDescription(todo.description)}>
               </button>
             </div>
             <div className="modal-body">
-            <input
+              <input
                 type="text"
                 className="form-control"
                 value={description}
@@ -41,13 +41,13 @@ const EditTodo = ({ todo, refreshTodos }) => {
               />
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" 
-              data-bs-dismiss="modal" 
-              onClick={() => setDescription(todo.description)}>Close
+              <button type="button" className="btn btn-secondary"
+                data-bs-dismiss="modal"
+                onClick={() => setDescription(todo.description)}>Close
               </button>
-              <button type="button" className="btn btn-warning" 
-              data-bs-dismiss="modal" 
-              onClick={() => editText(todo.todo_id)}>Edit
+              <button type="button" className="btn btn-warning"
+                data-bs-dismiss="modal"
+                onClick={() => editText(todo.todo_id)}>Edit
               </button>
             </div>
           </div>
