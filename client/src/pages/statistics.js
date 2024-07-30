7153246e0d7697b1044655634c63c828b3e9da3e
jesse <jesse21532@gmail.com>
2024-07-31 01:05:39 +1000

    Modified page to be controller for passing in different information from hook to each part of the page.

--- client/src/pages/statistics.js
+++ client/src/pages/statistics.js
@@ -1,14 +1,20 @@
 import React, { Fragment } from "react";
 import Header from "../components/Header";
+import UseSessionInfo from "../components/statistics/UseSessionHook"
+import BasicStatistics from "../components/statistics/BasicStatistics";
+import BarChart from "../components/statistics/BarChart";
+import HeatMap from "../components/statistics/HeatMap";
 
 function Statistics() {
+  const {dayCount, dayTime, weekCount, weekTime, monthCount, monthTime,
+    yearCount, yearTime, weekDailyHours, yearData} = UseSessionInfo();
+  const sessionData = { dayCount, dayTime, weekCount, weekTime, monthCount, monthTime, yearCount, yearTime };
   return (
     <Fragment>
       <Header />
-      <h1>Statistics</h1>
-      <div id=""></div>
-      <div id="distribution-graph"></div>
-      <div id="bar-graph"></div>
+      <BasicStatistics sessions={sessionData} />
+      <BarChart sessions={weekDailyHours} />
+      <HeatMap sessions={yearData} />
     </Fragment>
   )
 }
